1e32d7a9b66e83b2c74e72fb0409ba02f7426819
The_Name_Is_AK <71229713+AadityaKandel@users.noreply.github.com>
2023-04-28 15:54:41 +0545

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,199 @@
+from tkinter import *
+import tkinter.messagebox as tmsg
+import threading
+import subprocess
+
+
+root = Tk()
+root.minsize(385,450)
+root.maxsize(385,450)
+root.title('Calculator')
+
+
+# Creating Functions
+def create(frame_name,text):
+
+    button = Button(
+        frame_name,
+        text=text,
+        bg="black",
+        fg="white",
+        font="comicsansms 14 italic",
+        width=10,
+        command=lambda:l1.config(text=f"{l1['text']}{text}")
+    )
+
+    button.pack(side=LEFT)
+    return button
+
+def create1():
+
+    frame = Frame(
+        border=10,
+        bg="black"
+    )
+
+    return frame
+
+def options():
+    b17.config(text="Less",command=options1)
+    root.minsize(385,570)
+    root.maxsize(385,570)
+
+
+def options1():
+    b17.config(text="More",command=options)
+    root.minsize(385,450)
+    root.maxsize(385,450)
+
+def history():
+    try:
+        subprocess.Popen(['notepad.exe', 'history.txt'])
+    except:
+        pass
+
+def clear():
+    l1.config(text="")
+
+def cut():
+    l1.config(text=f"{l1['text'][0:-1]}")
+
+def small_bracket():
+    global check_small_brackets
+    if check_small_brackets==0:
+        l1.config(text=f"{l1['text']}(")
+        check_small_brackets = 1
+    elif check_small_brackets==1:
+        l1.config(text=f"{l1['text']})")
+        check_small_brackets = 0
+
+def big_bracket():
+    global check_big_brackets
+    if check_big_brackets==0:
+        l1.config(text=f"{l1['text']}[")
+        check_big_brackets = 1
+    elif check_big_brackets==1:
+        l1.config(text=f"{l1['text']}]")
+        check_big_brackets = 0
+
+def medium_bracket():
+    global check_medium_brackets
+    if check_medium_brackets==0:
+        l1.config(text=f"{l1['text']}{{")
+        check_medium_brackets = 1
+    elif check_medium_brackets==1:
+        l1.config(text=f"{l1['text']}}}")
+        check_medium_brackets = 0
+
+def equals():
+    big_number = l1['text']
+    replaced = big_number.replace('[','(').replace(']',')').replace('}',')').replace('{','(').replace('^','**')
+    try:
+        l1.config(text=f"{eval(replaced)}")
+        new_answer = l1['text']
+        f = open('history.txt','a')
+        f.write('\n'+big_number+'='+new_answer)
+        f.close()
+    except:
+        tmsg.showwarning('Warning','Wrong Input')
+
+def how_much():
+    l1.config(text=f"{l1['text']}^")
+
+# Creating Variables
+check_shift = 0
+check_small_brackets = 0
+check_big_brackets = 0
+check_medium_brackets = 0
+
+l1 = Label(
+    bg="black",
+    fg="white",
+    font="comicsansms 16 italic",
+    width=21,
+    state=DISABLED,
+)
+
+l1.pack()
+
+f1 = create1()
+f2 = create1()
+f3 = create1()
+f4 = create1()
+f5 = create1()
+f6 = create1()
+f7 = create1()
+f8 = create1()
+f9 = create1()
+
+
+
+# Frame 1
+b1 = create(f1,"History")
+b2 = create(f1,"C")
+b3 = create(f1,"(X)")
+f1.pack(anchor=W)
+
+# Frame 2
+b4 = create(f2,"1")
+b5 = create(f2,"2")
+b6 = create(f2,"3")
+f2.pack(anchor=W)
+
+# Frame 3
+b7 = create(f3,"4")
+b8 = create(f3,"5")
+b9 = create(f3,"6")
+f3.pack(anchor=W)
+
+# Frame 4
+b8 = create(f4,"7")
+b9 = create(f4,"8")
+b10 = create(f4,"9")
+f4.pack(anchor=W)
+
+# Frame 5
+b11 = create(f5,"0")
+b12 = create(f5,"+")
+b13 = create(f5,"-")
+f5.pack(anchor=W)
+
+# Frame 6
+b14 = create(f6,"*")
+b15 = create(f6,"/")
+b16 = create(f6,"=")
+f6.pack(anchor=W)
+
+# Frame 7
+b17 = create(f7,"More")
+f7.pack(anchor=W)
+
+# Frame 8
+b18 = create(f8,"^2")
+b19 = create(f8,"^3")
+b20 = create(f8,"^?")
+f8.pack(anchor=W)
+
+#Frame 9
+b21 = create(f9,"()")
+b22 = create(f9,"{}")
+b23 = create(f9,"[]")
+f9.pack(anchor=W)
+
+#Configures
+b17.config(
+    width=32,
+    command=options
+)
+b1.config(command=history)
+b2.config(command=clear)
+b3.config(command=cut)
+b21.config(command=small_bracket)
+b22.config(command=medium_bracket)
+b23.config(command=big_bracket)
+b16.config(command=equals)
+b20.config(command=how_much)
+
+
+root.config(bg="black")
+root.mainloop()
